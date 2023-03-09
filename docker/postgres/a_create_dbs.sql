4bb40b66b3ee0308b4372a3e943e55bd3e6bbd69
ghaggart <ghaggart@users.noreply.github.com>
2023-03-09 18:16:42 +0000

    updated test sql path

--- docker/postgres/a_create_dbs.sql
+++ docker/postgres/a_create_dbs.sql
@@ -1,5 +1,12 @@
+--create user phenomedb with encrypted password 'testpass';
+
+CREATE ROLE root superuser;
+ALTER ROLE root WITH LOGIN;
+CREATE database root;
+
 create database airflow;
 grant all privileges on database airflow to postgres;
+
 create database phenomedb_test;
 create database phenomedb;
 grant all privileges on database phenomedb_test to postgres;
