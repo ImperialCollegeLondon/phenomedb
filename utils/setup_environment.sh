56ded53f88b79ca82ffac5692a0c92de5ad8cef0
Gordon Haggart <ghaggart23@gmail.com>
2023-03-15 12:51:13 +0000

    testing readthedocs

---
+++ utils/setup_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 1. Create the necessary databases
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=testpass
+apt-get install postgresql postgresql-contrib redis-server
+psql -U $POSTGRES_USER -d $POSTGRES_PASSWORD -a -f ./docker/postgres/a_create_dbs.sql
