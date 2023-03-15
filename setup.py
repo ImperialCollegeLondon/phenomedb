54d1d3e0d4358fb5d37880958ec215cf7b5db4a1
Gordon Haggart <ghaggart23@gmail.com>
2023-03-15 09:19:58 +0000

    updated setup.py python_requires

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description='PhenomeDB is a platform for harmonisation and integration of multi-study metabolomics data.',
     author='Gordon A. Davies',
     author_email='phenomedb@proton.me',
-    python_requires='==3.9',
+    python_requires='<=3.9',
     setup_requires=['numpy'],
     install_requires=[
         'pendulum==2.0.5',
