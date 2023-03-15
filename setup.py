3502226e46289489822519bddb1976e4d3f0bc69
Gordon Haggart <ghaggart23@gmail.com>
2023-03-15 10:24:00 +0000

    added licence file COPYING and updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 #requires = open('./requirements.txt').read().strip().split('\n')
 setup(
     name='phenomedb',
-    version='0.9.7',
+    version='0.9.8',
     packages=['phenomedb','phenomedb.views'],
-    license='',
-    long_description='PhenomeDB is a platform for harmonisation and integration of multi-study metabolomics data.',
+    license='GPL-3.0-or-later',
+    description='PhenomeDB is a platform for harmonisation and integration of multi-study metabolomics data, that uses Postgres, Apache-Airflow, and Redis.',
+    long_description='PhenomeDB is a platform for harmonisation and integration of multi-study metabolomics data, that uses Postgres, Apache-Airflow, Redis, and the nPYc-Toolbox. Developed by the Phenome Centre at Imperial College London. Please see readthedocs for installation and usage. Copyright Imperial College London',
     author='Gordon A. Davies',
     author_email='phenomedb@proton.me',
-    python_requires='<=3.9',
+    python_requires='>3.8, <3.10',
     setup_requires=['numpy'],
     install_requires=[
         'pendulum==2.0.5',
@@ -27,5 +28,6 @@ setup(
         "pymzml[full]",
         'libchebipy'
     ],
-    include_package_data=True
+    include_package_data=True,
+    keywords=['phenomedb','metabolomics','LC-MS','NMR']
 )
