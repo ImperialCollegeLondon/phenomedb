1ff3a95eb82da2c13a4bace0a637e1c9065408d3
Gordon Haggart <ghaggart23@gmail.com>
2023-03-15 10:29:13 +0000

    updated the copyright notice

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['phenomedb','phenomedb.views'],
     license='GPL-3.0-or-later',
     description='PhenomeDB is a platform for harmonisation and integration of multi-study metabolomics data, that uses Postgres, Apache-Airflow, and Redis.',
-    long_description='PhenomeDB is a platform for harmonisation and integration of multi-study metabolomics data, that uses Postgres, Apache-Airflow, Redis, and the nPYc-Toolbox. Developed by the Phenome Centre at Imperial College London. Please see readthedocs for installation and usage. Copyright Imperial College London',
+    long_description='PhenomeDB is a platform for harmonisation and integration of multi-study metabolomics data, that uses Postgres, Apache-Airflow, Redis, and the nPYc-Toolbox. Developed by the Phenome Centre at Imperial College London. Please see readthedocs for installation and usage. Copyright 2023 Imperial College London',
     author='Gordon A. Davies',
     author_email='phenomedb@proton.me',
     python_requires='>3.8, <3.10',
